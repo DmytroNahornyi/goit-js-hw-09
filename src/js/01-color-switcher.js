dda47e70b97a81f20e0eb2943fa4ad4fe9b664dc
Dmytro Nahornyi <d3049999@gmail.com>
2023-01-25 18:20:38 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
 
+stopBtnEl.disabled = true;
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtnEl.addEventListener('click', () => {
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
+
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+});
+
+stopBtnEl.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+})
